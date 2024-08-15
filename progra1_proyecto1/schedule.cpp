d546a589ae371b4120183ed4391a69579a10250b
Fwolagos <aantonypv@gmail.com>
2024-08-14 19:47:05 -0600

    Feat: finished the Schedule class
    I created the schedule class and your gets, sets and costructors

---
+++ progra1_proyecto1/schedule.cpp
@@ -0,0 +1,70 @@
+#include "schedule.h"
+
+
+#pragma region Constructors
+Schedule::Schedule(string date, string startHour, string endHour, Movie movie, Room room) {
+	this->date = date;
+	this->startHour = startHour;
+	this->endHour = endHour;
+	this->movie = movie;
+	this->room = room;
+
+}
+Schedule::Schedule() {
+	date = "00/00/00";
+	startHour = "24:60";
+	endHour = "24:60";
+	movie = Movie();
+	room = Room();
+}
+Schedule::~Schedule() {
+
+}
+#pragma endregion
+
+#pragma region Sets
+void Schedule::setDate(string date) {
+	this->date = date;
+}
+
+void Schedule::setStartHour(string startHour) {
+	this->startHour = startHour;
+}
+
+void Schedule::setEndHour(string endHour) {
+	this->endHour = endHour;
+}
+
+void Schedule::setMovie(Movie movie) {
+	this->movie = movie;
+}
+
+void Schedule::setRoom(Room room) {
+	this->room = room;
+}
+#pragma endregion
+
+#pragma region Gets
+string Schedule::getDate() {
+	return date;
+}
+
+string Schedule::getStartHour() {
+	return startHour;
+}
+
+string Schedule::getEndHour() {
+	return endHour;
+}
+
+Movie Schedule::getMovie() {
+	return movie;
+}
+
+Room Schedule::getRoom() {
+	return room;
+}
+#pragma endregion
+
+
+
---
+++ progra1_proyecto1/schedule.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <iostream>
+#include "movie.h"
+#include "room.h"
+
+using namespace std;
+
+class Schedule
+{
+private:
+    string date;
+    string startHour;
+    string endHour;
+    Movie movie; 
+    Room room; 
+
+public:
+    Schedule(string, string, string, Movie, Room);
+    Schedule();
+    ~Schedule();
+
+    void setDate(string);
+    void setStartHour(string);
+    void setEndHour(string);
+    void setMovie(Movie);
+    void setRoom(Room);
+
+    string getDate();
+    string getStartHour();
+    string getEndHour();
+    Movie getMovie();
+    Room getRoom();
+
+};
+
