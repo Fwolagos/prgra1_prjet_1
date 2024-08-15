0a7c0ea3bdab10aa09f10052f1669555792e14d4
Fwolagos <aantonypv@gmail.com>
2024-08-14 18:51:44 -0600

    Feat: class room finished
    i'm finishing creating the gets and sets of the room class

--- progra1_proyecto1/room.cpp
+++ progra1_proyecto1/room.cpp
@@ -5,7 +5,7 @@
 #pragma region Builders
 Room::Room(int id, double price) {
 	this->id = id;
-	seatsNumber = 100;
+	seatsNumber = MATRIX_SEAT * MATRIX_SEAT;
 	this->price = price;
 	for (int i = 0; i < MATRIX_SEAT * MATRIX_SEAT; i++) {
 		roomSeats[i / MATRIX_SEAT][i % MATRIX_SEAT].setId(i);
@@ -14,8 +14,8 @@ Room::Room(int id, double price) {
 }
 
 Room::Room() {
-	id = rand() % 100;
-	seatsNumber = 100;
+	id = rand() % 100000;
+	seatsNumber = MATRIX_SEAT * MATRIX_SEAT;
 	for (int i = 0; i < MATRIX_SEAT * MATRIX_SEAT; i++) {
 		roomSeats[i / MATRIX_SEAT][i % MATRIX_SEAT].setId(i);
 		roomSeats[i / MATRIX_SEAT][i % MATRIX_SEAT].setState("available");
@@ -27,12 +27,40 @@ Room::~Room() {}
 
 
 #pragma region Gets
+int Room::getId() {
+	return id;
+}
 
+int Room::getSeatsNumber() {
+	return seatsNumber;
+}
+
+double Room::getPrice() {
+	return price;
+}
+
+Seat Room::getroomSeats(int row, int column) {//row and column
+	return roomSeats[row][column];
+}
 #pragma endregion
 
 
 #pragma region Sets
+void Room::setId(int id) {
+	this->id = id;
+}
 
+void Room::setSeatsNumber(int seatsNumber) {
+	this->seatsNumber = seatsNumber;
+}
+
+void Room::setPrice(double price) {
+	this->price = price;
+}
+
+void Room::setroomSeats(Seat roomSeat, int row, int column) {//row and column
+	this->roomSeats[row][column] = roomSeat;
+}
 #pragma endregion
 
 
