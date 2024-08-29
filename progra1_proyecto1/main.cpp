97d8e84bf407aeaf5f1d4af1ba837aa88dcdb8f4
Fwolagos <aantonypv@gmail.com>
2024-08-28 21:41:15 -0600

    Feat: created the documentation about 'header' function

--- progra1_proyecto1/main.cpp
+++ progra1_proyecto1/main.cpp
@@ -2,6 +2,29 @@
 using namespace std;
 
 #pragma region Functions_Declaration
+/**
+ * @brief Imprime títulos decorados en la consola.
+ *
+ * Esta función imprime títulos decorados en la consola utilizando asteriscos
+ * (`*`) como bordes. El título impreso depende del tipo especificado en el
+ * parámetro `title`. Se limpia la pantalla al inicio de la función utilizando
+ * `system("cls")` en sistemas Windows para mantener la salida del programa ordenada.
+ *
+ * @param title Una cadena de texto que especifica el tipo de título que se desea
+ *              imprimir. Debe ser uno de los siguientes valores:
+ *              - `"principal"`: Imprime el título "Bienvenidos al Cine Fwolagos".
+ *              - `"archivo"`: Imprime el título "Acerca del Cine de Fwolagos".
+ *              - `"mantenimiento"`: Imprime el título "Mantenimiento Cine Fwolagos".
+ *              - `"reserva"`: Imprime el título "Reservaciones Cine Fwolagos".
+ *              - `"venta"`: Imprime el título "Ventas Cine Fwolagos".
+ *
+ * @note La función utiliza `system("cls")` para limpiar la pantalla. Esto es específico
+ *       de sistemas Windows. En otros sistemas operativos, considera utilizar
+ *       `system("clear")` en su lugar.
+ *
+ * @warning La función no maneja errores si el tipo de título proporcionado no
+ *          coincide con ninguno de los valores esperados.
+ */
 void header(string);
 #pragma endregion
 
