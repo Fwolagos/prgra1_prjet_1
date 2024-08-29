eac3ad9f4f38e3924c4514d18ef01a4b77912f7d
Fwolagos <aantonypv@gmail.com>
2024-08-28 21:23:50 -0600

    Feat: Added "header" funciton to print decorated titles
    The "header" function has been implemented to diasplay decorated titles
    based on the espesific type. supported types are:
    
    - principal
    - archivo
    - mantenimiento
    - reserva
    - venta
    
    Each type prints a title with a decorated box design. Addicionally, the
    screen is cleared at the start of the fuctions using "system("cls").

--- progra1_proyecto1/main.cpp
+++ progra1_proyecto1/main.cpp
@@ -1,6 +1,91 @@
 #include <iostream>
 using namespace std;
 
+#pragma region Functions_Declaration
+void header(string);
+#pragma endregion
+
+
 int main() {
 	cout << "ejemplo para andy ";
 }
+
+#pragma region Functions_Definition 
+void header(string title) {
+	/// Tipos de titulos para imprimir
+	/// titulo: principal
+	/// titulo: archivo
+	/// titulo: mantenimiento
+	/// titulo: reserva
+	/// titulo: venta
+	
+	system("cls");
+
+	/// Titulo: principal
+	if (title == "principal") {
+		cout << endl;
+		cout << "***************************************************************";
+		cout << endl;
+		cout << "*                Bienvenidos al Cine Fwolagos                 *" << endl;
+		cout << endl;
+		cout << "***************************************************************";
+		cout << endl;
+
+		return;
+	}
+	/// Titulo: archivo
+	if (title == "archivo") {
+		cout << endl;
+		cout << "***************************************************************";
+		cout << endl;
+		cout << "*                Acerca del  Cine de Fwolagos                 *" << endl;
+		cout << endl;
+		cout << "***************************************************************";
+		cout << endl;
+
+		return;
+	}
+	/// Titulo: mantenimiento
+	if (title == "mantenimiento") {
+		cout << endl;
+		cout << "***************************************************************";
+		cout << endl;
+		cout << "*                Mantenimiento Cine Fwolagos                  *" << endl;
+		cout << endl;
+		cout << "***************************************************************";
+		cout << endl;
+
+		return;
+	}
+	/// Titulo: reserva
+	if (title == "reserva") {
+		cout << endl;
+		cout << "***************************************************************";
+		cout << endl;
+		cout << "*                Reservaciones Cine Fwolagos                  *" << endl;
+		cout << endl;
+		cout << "***************************************************************";
+		cout << endl;
+
+		return;
+	}
+	/// Titulo: venta
+	if (title == "venta") {
+		cout << endl;
+		cout << "***************************************************************";
+		cout << endl;
+		cout << "*                     Ventas Cine Fwolagos                    *" << endl;
+		cout << endl;
+		cout << "***************************************************************";
+		cout << endl;
+
+		return;
+	}
+}
+
+
+
+
+
+
+#pragma endregion
