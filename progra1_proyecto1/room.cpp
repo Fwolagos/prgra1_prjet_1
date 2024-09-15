c8bfacdf698fe0f95854c0bbab869b3070f46d7a
Fwolagos <aantonypv@gmail.com>
2024-09-15 16:19:19 -0600

    Fix: Correction in the Room class
    In the room class when the seats were initialized they were initialized by putting the Available status but, it turns out that I had put it in lower case and in all the other parts I had done it in upper case so when there was a function that depended on locating “Available” in this way it failed.

--- progra1_proyecto1/room.cpp
+++ progra1_proyecto1/room.cpp
@@ -9,7 +9,7 @@ Room::Room(int id, double price) {
 	this->price = price;
 	for (int i = 0; i < MATRIX_SEAT * MATRIX_SEAT; i++) {
 		roomSeats[i / MATRIX_SEAT][i % MATRIX_SEAT].setId(i);
-		roomSeats[i / MATRIX_SEAT][i % MATRIX_SEAT].setState("available");
+		roomSeats[i / MATRIX_SEAT][i % MATRIX_SEAT].setState("Available");
 	}
 }
 
@@ -19,7 +19,7 @@ Room::Room() {
 	seatsNumber = MATRIX_SEAT * MATRIX_SEAT;
 	for (int i = 0; i < MATRIX_SEAT * MATRIX_SEAT; i++) {
 		roomSeats[i / MATRIX_SEAT][i % MATRIX_SEAT].setId(i);
-		roomSeats[i / MATRIX_SEAT][i % MATRIX_SEAT].setState("available");
+		roomSeats[i / MATRIX_SEAT][i % MATRIX_SEAT].setState("Available");
 	}
 }
 
